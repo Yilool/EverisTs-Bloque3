d0713c2d3a085d666c73417cb76818c928dfd813
Yilool <20010917chen@gmail.com>
2020-11-12 13:35:16 +0100

    2 files modified

--- index.ts
+++ index.ts
@@ -4,3 +4,24 @@
 // Para terminar, asignad un objeto literal (sin clase) a la variable "user" (con los datos que queráis pero que no dé erroes de tipo) y luego realizad dos llamadas a "getData". La primera llamada debe recibir por parámetros el valor de la propiedad "name" del usuario y la segunda el valor de "phone".
 // OPCIONAL: Dentro de "getData" añadid un condicional con una guarda para diferenciar cuando reciba un tipo "number" y sacar por consola mensajes distintos para las dos llamadas anteriormente mencionadas.
 // Ánimo que ya lo tenéis!
+
+import { UserData } from "./user-data";
+
+var user: UserData;
+
+const getData = <T>(arg: T) => {
+  if (typeof arg === "number") {
+    console.log(`${arg} es un numero`);
+  } else {
+    console.log(arg);
+  }
+};
+
+user = {
+  name: "Yi",
+  age: 19,
+  phone: 34
+};
+
+getData<string>(user.name);
+getData<number>(user.phone);
